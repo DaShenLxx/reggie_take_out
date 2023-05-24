9afbd39d2f087bf3fb0c8ae949a8b9d7bd29d1ab
DaShenLxx <109137485+DaShenLxx@users.noreply.github.com>
2023-05-24 18:42:09 +0800

    Update SMSUtils.java

--- src/main/java/com/example/reggie_take_out/utils/SMSUtils.java
+++ src/main/java/com/example/reggie_take_out/utils/SMSUtils.java
@@ -20,7 +20,7 @@ public class SMSUtils {
 	 * @param param 参数
 	 */
 	public static void sendMessage(String signName, String templateCode,String phoneNumbers,String param){
-		DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "LTAI5t7472si7MpeyCmHkP47", "2YfQOoYRSZXjnF63LhacyHvNbRKWnz");
+		DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "", "");
 		IAcsClient client = new DefaultAcsClient(profile);
 
 		SendSmsRequest request = new SendSmsRequest();
